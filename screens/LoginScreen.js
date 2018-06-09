d1fd88e83d1a6431f422976e560584b20806b9e2
Hasiru Kavishan <bgh.kavishan@gmail.com>
2018-06-09 15:28:19 +0530

    remove header in login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -16,56 +16,39 @@ class LoginScreen extends Component{
     }
 
     render(){
-        return(
-            
+        return(       
             <Container>
-            <Content/>
-                <View style={styles.container}>
-                    <View style={styles.getStartedContainer}>
-                        <View style={styles.logo} >
-                            <Image
-                            style={{marginTop:30,width: 200, height: 70,justifyContent: 'center',alignItems: 'center',}}
-                                source={
-                                    require('../assets/images/instagram-text-logo.png')
-                                }
-                            />
-                            </View> 
-                        </View>
-                </View>
-
-                <View style={{marginTop:-30}}>
-                    <View style={styles.inputFeildOne}>
-                        <Item regular style={{borderRadius:4,width:270,height:40}}>
+                <Header style={{width:0, height:0}}></Header>
+                <Content>
+                    <View style={{alignItems:'center',justifyContent:'center'}}>
+                    <Image
+                    style={{marginTop:30,width: 200, height: 70,justifyContent: 'center',alignItems: 'center',justifyContent:'center'}}
+                        source={
+                            require('../assets/images/instagram-text-logo.png')
+                        }
+                    />
+                    </View>
+                    
+                    <View style={{alignItems:'center',justifyContent:'center'}}>
+                        <Item regular style={{marginTop:30,borderRadius:4,width:270,height:40,padding:10}}>
                             <Input style={{opacity:.54,fontSize:13}} placeholder='Phone number, username or email' />
                         </Item>
-                    </View>
 
-                    <View style={styles.inputFeildTwo}>
-                        <Item regular style={{borderRadius:4,width:270,height:40}}>
+                        <Item regular style={{marginTop:15,borderRadius:4,width:270,height:40}}>
                             <Input style={{opacity:.54,fontSize:13}} placeholder='Password' />
-                        </Item>
-                    </View>
+                        </Item>>
 
-                    {/* <View>
-                        
-                            <Text style={{justifyContent:'center',textAlign:'center',marginTop:15,fontSize:12}}>Forget password?</Text>
-                        
-                    </View> */}
-
-                    <View style={styles.button}>
-                        <Button onPress={()=>this.swithcMainScreen()} block info style={{borderRadius:4,width:270,height:40,backgroundColor:'#15667e'}}>
+                        <Button onPress={()=>this.swithcMainScreen()} block info style={{marginTop:25,marginLeft:26,borderRadius:4,width:270,height:40,backgroundColor:'#15667e'}}>
                             <Text style={{alignItems:'center',justifyContent:'center'}}>Login</Text>
                         </Button>
                     </View>
-                </View>
-        
+                
+                </Content>
                 <Footer style={{height:40}}>
-                <Text style={{justifyContent:'center',textAlign:'center',marginTop:15,opacity:.54,fontSize:12}}>Don't have an account? </Text>
-                <Text  style={{justifyContent:'center',textAlign:'center',marginTop:15,fontSize:12,color:'#45b2c5'}}>Sign Up</Text>
+                    <Text style={{justifyContent:'center',textAlign:'center',marginTop:15,opacity:.54,fontSize:12}}>Don't have an account? </Text>
+                    <Text  style={{justifyContent:'center',textAlign:'center',marginTop:15,fontSize:12,color:'#45b2c5'}}>Sign Up</Text>
                 </Footer>
-
             </Container>
-            
         )
     }
 }
@@ -81,42 +64,5 @@ export default screens=createStackNavigator({
     headerMode:'none',
     navigationOptions:{
         headerMode:'none',
-    }
-})
-const styles = StyleSheet.create({
-    container: {
-        flex: 6,
-        backgroundColor: '#fff',
-        marginTop:10,
-    },
-    getStartedContainer: {
-    alignItems: 'center',   
-    },
-    inputFeildOne:{
-        flex: 2,
-        marginTop:-820,
-        alignItems: 'center',
-        justifyContent:'center', 
-    },
-    inputFeildTwo:{
-        flex:1,
-        marginTop:-700,
-        alignItems: 'center',
-        justifyContent:'center', 
-    },
-    button:{
-        flex: 0,
-        marginLeft:26,
-        marginTop:-400,
-        alignItems: 'center',
-        justifyContent:'center', 
-    },
-
-    logo:{
-        alignItems:'center',
-        width:50,
-        height:50,
-        justifyContent:'center',
-        flexGrow:0,
     }
 })
